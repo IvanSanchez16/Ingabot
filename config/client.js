b87761ac279bef16f37e127a275d1f4a78b9ae26
Ivan Sanchez <ivanhumberto_16@hotmail.com>
2020-08-25 23:01:16 -0600

    Mensajes y respuestas
    Terminados y funcionando

--- config/client.js
+++ config/client.js
@@ -1,11 +1,15 @@
 import { Client } from "discord.js";
 import { 
+    Saludo,
     MentadaDeMadre,
     band,
     quien,
     PutoElUltimo,
-    TePregunto
-    } from "../methods/messages.js"
+    TePregunto,
+    Pulicuata,
+    MensajeSolis
+    }
+ from "../methods/messages.js"
 
 const client = new Client();
 
@@ -17,6 +21,18 @@ client.on('message',(msg) => {
         }
         if ( msg.content.substr(0, 3) === 'put' && msg.content.length <= 10 && msg.content.length >= 6 ){
             PutoElUltimo(msg);
+            return;
+        }
+        if ( msg.content === 'hola puli' ){
+            Saludo(msg);
+            return;
+        }
+        if ( msg.author.username === 'LuiSolis' && Math.floor(Math.random() * 5) == 4){
+            MensajeSolis(msg);
+            return;
+        }
+        if ( msg.content.toLocaleLowerCase().includes('pulicuata') ){
+            Pulicuata(msg);
             return;
         }
         if ( Math.floor(Math.random() * 10) == 4 ){
--- methods/messages.js
+++ methods/messages.js
@@ -4,11 +4,39 @@ const insultos = [
     "Simon simon ya callate el hocico pues",
     "Chingas a tu madre"
 ];
+
+const respPulicuata = [
+    'Puras mamadas decia el fifo',
+    'Es mentira esa madre',
+    'Pinche fifo'
+];
+
+const saludos = [
+    'Que rollo',
+    'Hola!',
+    'Buenas!'
+];
+
+const respSolis = [
+    "Te quiero Solis",
+    'Pinche cerda de mierda',
+    'Pinche foca celulitica',
+    'Ya me cambiaste por el pendejo del Canelitas',
+    'Te gustan de rancho',
+    'Chinga tu madre gordo de mierda',
+    '* Zape *'
+];
+
 var contador = 0;
 var puto = null;
 
 var band = false;
 var quien = null;
+
+function Saludo(msg){
+    let number = Math.floor(Math.random() * saludos.length);
+    msg.channel.send( saludos[number] );
+}
 
 function MentadaDeMadre(msg) {
     let number = Math.floor(Math.random() * insultos.length);
@@ -44,4 +72,14 @@ function PutoElUltimo(msg){
     }
 }
 
-export { quien, band, MentadaDeMadre, PutoElUltimo, TePregunto };
+function Pulicuata(msg){
+    let number = Math.floor(Math.random() * respPulicuata.length);
+    msg.channel.send( respPulicuata[number] );
+}
+
+function MensajeSolis(msg){
+    let number = Math.floor(Math.random() * respSolis.length);
+    msg.channel.send( respSolis[number] );
+}
+
+export { quien, band, MentadaDeMadre, PutoElUltimo, TePregunto, Pulicuata, MensajeSolis, Saludo };
